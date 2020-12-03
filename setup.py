602829c832fe7a9b397a6c9f33d0cf27f21bb3e2
hasegawa akiyuki <hase.eeicm@gmail.com>
2020-12-03 16:47:55 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,12 @@ from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
 from torch.utils.hipify import hipify_python
 
 torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
+if "DEBUG" in os.environ and os.environ['DEBUG']:
+    with open(os.path.join(os.environ['HOME'], 'TORCH_VER_INSTALL'), 'wt') as wt: 
+        wt.write(f"\n\ntorch version: {torch_ver}\n\n")
 assert torch_ver >= [1, 5], "Requires PyTorch >= 1.5"
+
+
 
 
 def get_version():
